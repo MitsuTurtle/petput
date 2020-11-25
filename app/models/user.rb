6958f0455d7a738ae12505e7e2349f76f8838fbe
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-25 17:20:28 +0900

    nicknameのバリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
+    validates :nickname, uniqueness: { case_sensitive: true }
+    # パスワードは半角英数字混合での入力が必須
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-    validates :nickname, uniqueness: true
     validates :password, format: { with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' }
   end
 end
