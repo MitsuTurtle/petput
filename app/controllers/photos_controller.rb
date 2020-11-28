684ea33f9ba09d7e346ce4a77241934935c3b156
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-11-28 15:37:42 +0900

    Active Strageの導入

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,4 +1,21 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:caption, :iamge).merge(user_id: current_user.id)
+  end
+
+
 end
