5293c0478529c2354c68dd44481ec5d9bdc528ad
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-12-01 21:12:44 +0900

    アソシエーションを定義

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :photos
+  has_many :comments
 
   with_options presence: true do
     validates :nickname, uniqueness: { case_sensitive: true }
