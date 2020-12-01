c9e444de739b18508573ba67c711bfa3b987cde0
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-12-01 22:25:55 +0900

    ルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "photos#index"
-  resources :photos, only: [:index, :new, :create, :show] do
+  resources :photos, only: [:index, :new, :create, :show, :destroy] do
     resources :comments, only: :create
   end
 end
