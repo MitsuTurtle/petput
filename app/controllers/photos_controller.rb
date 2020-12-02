64efb38687f32a64dd61148b96a1f193c8c70998
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-12-02 21:16:21 +0900

    photosコントローラーにedit、updateメソッドを定義

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,18 +9,31 @@ class PhotosController < ApplicationController
     @photo = Photo.new
   end
 
-  def show
-    @photo = Photo.find(params[:id])
-    @comment = Comment.new
-    @comments = @photo.comments.includes(:user)
-  end
-
   def create
     @photo = Photo.new(photo_params)
     if @photo.save
       redirect_to root_path
     else
       render :new
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    @comment = Comment.new
+    @comments = @photo.comments.includes(:user)
+  end
+
+  def edit
+    @photo = Photo.find(params[:id])
+  end
+
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to photo_path
+    else
+      render :edit
     end
   end
 
@@ -35,4 +48,5 @@ class PhotosController < ApplicationController
   def photo_params
     params.require(:photo).permit(:image, :caption, :category_id).merge(user_id: current_user.id)
   end
+
 end
