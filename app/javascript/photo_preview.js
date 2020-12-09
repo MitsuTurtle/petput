2976f41048327ce222d5a442e6306fe84932eb6c
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-12-09 22:56:36 +0900

    jsファイルをリファクタリング

--- app/javascript/photo_preview.js
+++ app/javascript/photo_preview.js
@@ -2,16 +2,9 @@ if (document.URL.match(/new/) || document.URL.match(/edit/)) {
 
   document.addEventListener('DOMContentLoaded', function( ){
     const ImageList = document.getElementById('image-list');
-    document.getElementById('file-button').addEventListener('change', function(e){
-      // 画像が表示されている場合のみ、すでに存在している画像を削除する
-      const imageContent = document.querySelector("img");
-      if (imageContent) {
-        imageContent.remove();
-      };
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-
+    // 選択した画像を表示する関数
+    const createImageHTML = (blob) => {
       // 画像を表示するためのdiv要素を生成
       const imageElement = document.createElement('div')
 
@@ -23,6 +16,19 @@ if (document.URL.match(/new/) || document.URL.match(/edit/)) {
       // 生成したHTMLの要素をブラウザに表示させる
       imageElement.appendChild(blobImage);
       ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('file-button').addEventListener('change', function(e){
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector("img");
+      if (imageContent) {
+        imageContent.remove();
+      };
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
     });
 });
 }
