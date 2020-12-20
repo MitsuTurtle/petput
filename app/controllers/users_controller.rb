375c3f499ade365b20e3a830f17b794b0b2f7145
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2020-12-20 17:10:53 +0900

    usersコントローラーにsearch_category_photoメソッドを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  before_action :search_category_photo, only: :show
+
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
     @photos = user.photos
   end
+
+  private
+
+  def search_category_photo
+    @q = Photo.ransack(params[:q])
+  end
+
 end
