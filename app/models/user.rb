2011482a32b95253c3e3acda0c8b4fdc9f9b1dcf
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-01-13 21:59:01 +0900

    Userモデルにdependent: :destroyを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :photos
-  has_many :comments
+  has_many :photos, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   has_many :votes, dependent: :destroy
   has_many :voted_photos, through: :votes, source: :photo
