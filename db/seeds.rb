37d8ebad9353ba78a4703d28a9c937a4bcad46f9
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-01-13 21:58:41 +0900

    seedsファイルを編集（写真・ユーザー登録用）

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# ユーザー10名追加
+# 10.times do |num|
+#   user = User.new(nickname: Faker::Internet.username, email: Faker::Internet.free_email, password: "11111a", profile: Faker::Lorem.sentence(word_count: 15))
+#   user.avatar.attach(io: File.open(Rails.root.join("app/assets/images/avatar/avatar#{num + 1}.jpeg")),filename: "avatar#{num + 1}.jpeg")
+#   user.save
+# end
+
+# 写真追加
+# カメ写真
+22.times do |num|
+  photo = Photo.new(caption: "#かめ #カメ #亀 #turtle\n\nカメちゃんです❗", category_id: 5, user_id: 31)
+  photo.image.attach(io: File.open(Rails.root.join("app/assets/images/turtle#{num+1}.jpg")),filename: "turtle#{num+1}.jpg")
+  photo.save
+end
+
+# 猫写真
+# photo = Photo.new(caption: "#ねこ #ネコ #猫 #cat\n\n\n がんばって❗", category_id: 2, user_id: 34)
+# photo.image.attach(io: File.open(Rails.root.join("app/assets/images/cat1.jpg")),filename: "cat1.jpg")
+# photo.save
