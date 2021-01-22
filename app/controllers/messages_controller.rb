8851753825e8ed745d8ba5f225ddfd0d063f212f
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-01-22 22:09:32 +0900

    ダイレクトメッセージをJavaScriptで非同期通信化

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,7 +4,8 @@ class MessagesController < ApplicationController
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to "/rooms/#{@message.room_id}"
+      # redirect_to "/rooms/#{@message.room_id}"
+      render "create.js.erb"
     else
       redirect_to(fallback_location: root_path)
     end
