46e7842fb9751ab58adc7bbf55ae15acf336a6da
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-01-23 18:52:27 +0900

    ペットのカテゴリーを修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,12 +3,13 @@ class Category < ActiveHash::Base
     { id: 0, name: '--' },
     { id: 1, name: '犬' },
     { id: 2, name: '猫' },
-    { id: 3, name: '鳥' },
-    { id: 4, name: '魚' },
+    { id: 3, name: '哺乳類（犬・猫以外）' },
+    { id: 4, name: '鳥類' },
     { id: 5, name: '爬虫類' },
     { id: 6, name: '両生類' },
-    { id: 7, name: '昆虫' },
-    { id: 8, name: 'その他' }
+    { id: 7, name: '魚類' },
+    { id: 8, name: '節足動物' },
+    { id: 9, name: 'その他の動物' }
   ]
 
   include ActiveHash::Associations
