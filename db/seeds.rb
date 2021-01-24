0ed499bec58ee9ffc59ee81e7250198c545b9e32
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-01-24 09:36:39 +0900

    seed.rbで写真をまとめて投稿

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,13 +15,29 @@
 
 # 写真追加
 # カメ写真
-22.times do |num|
-  photo = Photo.new(caption: "#かめ #カメ #亀 #turtle\n\nカメちゃんです❗", category_id: 5, user_id: 31)
-  photo.image.attach(io: File.open(Rails.root.join("app/assets/images/turtle#{num+1}.jpg")),filename: "turtle#{num+1}.jpg")
-  photo.save
-end
+# 22.times do |num|
+#   photo = Photo.new(caption: "#かめ #カメ #亀 #turtle\n\nカメちゃんです❗", category_id: 5, user_id: 31)
+#   photo.image.attach(io: File.open(Rails.root.join("app/assets/images/turtle#{num+1}.jpg")),filename: "turtle#{num+1}.jpg")
+#   photo.save
+# end
 
 # 猫写真
-# photo = Photo.new(caption: "#ねこ #ネコ #猫 #cat\n\n\n がんばって❗", category_id: 2, user_id: 34)
-# photo.image.attach(io: File.open(Rails.root.join("app/assets/images/cat1.jpg")),filename: "cat1.jpg")
-# photo.save
+# 10.times do |num|
+#   photo = Photo.new(caption: "#ねこ #ネコ #猫 #cat\n\n\n 猫ちゃんです❗", category_id: 2, user_id: 34)
+#   photo.image.attach(io: File.open(Rails.root.join("app/assets/images/cat#{num+1}.jpg")),filename: "cat#{num+1}.jpg")
+#   photo.save
+# end
+
+# 節足動物写真
+# 10.times do |num|
+#   photo = Photo.new(caption: "#虫 #むし #ムシ \n\n\n 虫です❗", category_id: 8, user_id: 35)
+#   photo.image.attach(io: File.open(Rails.root.join("app/assets/images/arthropod#{num+1}.jpg")),filename: "arthropod#{num+1}.jpg")
+#   photo.save
+# end
+
+# うさぎ写真
+10.times do |num|
+  photo = Photo.new(caption: "#うさぎ #ウサギ #rabbit \n\n\n うさぎです❗", category_id: 3, user_id: 32)
+  photo.image.attach(io: File.open(Rails.root.join("app/assets/images/rabbit#{num+1}.jpg")),filename: "rabbit#{num+1}.jpg")
+  photo.save
+end
