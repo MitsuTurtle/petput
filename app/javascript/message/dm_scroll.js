b43f78b1c0a7fe7e145eb36575d048dc99cc8474
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-02-20 16:05:55 +0900

    ダイレクトメッセージのビューを修正

--- app/javascript/message/dm_scroll.js
+++ app/javascript/message/dm_scroll.js
@@ -1,3 +1,4 @@
 const target = document.getElementById('scroll-inner');
 // target.scrollIntoView(false);
-target.scrollIntoView({behavior: "smooth", block: "end"});
+// target.scrollIntoView({behavior: "smooth", block: "end"});
+target.scrollIntoView({behavior: "auto", block: "end"});
