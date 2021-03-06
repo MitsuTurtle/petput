472f87d3830edddf1cf00d9ec2105d22d1dfc8e6
MitsuTurtle <71766878+MitsuTurtle@users.noreply.github.com>
2021-03-06 11:55:43 +0900

    Photoモデルのテストコード

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -3,31 +3,31 @@ require 'rails_helper'
 RSpec.describe Photo, type: :model do
   before do
     @photo = FactoryBot.build(:photo)
-    @photo.image = fixture_file_upload('/files/test_image.png')
+    @photo.image = fixture_file_upload('/files/test_image.jpg')
   end
 
   describe '写真の投稿' do
-    context '写真の投稿がうまくいくとき' do
-      it 'すべての要素が正しく入力されていると投稿できる' do
+    context '写真の投稿がうまくいく場合' do
+      it 'すべての要素が正しく入力されていると投稿できること' do
         expect(@photo).to be_valid
       end
     end
 
-    context '写真の投稿がうまくいかないとき' do
-      it '画像ファイルが未選択だと投稿できない' do
+    context '写真の投稿がうまくいかない場合' do
+      it '画像ファイルが未選択だと投稿できないこと' do
         @photo.image = nil
         @photo.valid?
-        expect(@photo.errors.full_messages).to include("Image can't be blank")
+        expect(@photo.errors.full_messages).to include("画像を選択してください")
       end
       it 'キャプションが未入力だと投稿できない' do
         @photo.caption = ''
         @photo.valid?
-        expect(@photo.errors.full_messages).to include("Caption can't be blank")
+        expect(@photo.errors.full_messages).to include("キャプションを入力してください")
       end
       it 'カテゴリーが未選択だと出品できない' do
         @photo.category_id = 0
         @photo.valid?
-        expect(@photo.errors.full_messages).to include('Category must be other than 0')
+        expect(@photo.errors.full_messages).to include('カテゴリーを選択してください')
       end
     end
   end
